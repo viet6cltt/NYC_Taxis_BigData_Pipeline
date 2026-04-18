2648f4c41d7fb7cd4a1af8dbc99a7fe02c146d64
VietPhan <phanvanviet70@gmail.com>
2026-04-19 06:00:03 +0700

    feat(ingest): add scripts to run batch, fix some errors in batch ingestion

--- apps/ingestion/batch/historical_to_bronze/app/config.py
+++ apps/ingestion/batch/historical_to_bronze/app/config.py
@@ -7,3 +7,5 @@ OUTPUT_PATH = os.getenv("OUTPUT_PATH", "s3a://lakehouse/bronze/nyc-taxi")
 MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://minio-api.minio.svc.cluster.local:9000")
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
+
+YEAR = os.getenv("YEAR", "2024")
--- apps/ingestion/common/bronze_mapper.py
+++ apps/ingestion/common/bronze_mapper.py
@@ -1,4 +1,4 @@
-from pyspark.sql import DateFrame
+from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 from common.bronze_contract import BRONZE_COLUMNS
 from common.constants import (
@@ -6,15 +6,13 @@ from common.constants import (
     DROPOFF_DATETIME_FIELD
 )
 
-def map_to_bronze(df: DateFrame) -> DateFrame:
+def map_to_bronze(df: DataFrame) -> DataFrame:
     return (
-        df.select(
-            F.col("VendorID").alias("vendor_id"),
-            F.col(PICKUP_DATETIME_FIELD).alias("pickup_datetime"),
-            F.col(DROPOFF_DATETIME_FIELD).alias("dropoff_datetime"),
-            F.col("RatecodeID").alias("rate_code_id"),
-            F.col("PULocationID").alias("pulocation_id"),
-            F.col("DOLocationID").alias("dolocation_id"),
-            F.col("Airport_fee").alias("airport_fee")
-        )
+        df.withColumnRenamed("VendorID", "vendor_id")
+          .withColumnRenamed(PICKUP_DATETIME_FIELD, "pickup_datetime")
+          .withColumnRenamed(DROPOFF_DATETIME_FIELD, "dropoff_datetime")
+          .withColumnRenamed("RatecodeID", "rate_code_id")
+          .withColumnRenamed("PULocationID", "pulocation_id")
+          .withColumnRenamed("DOLocationID", "dolocation_id")
+          .withColumnRenamed("Airport_fee", "airport_fee")
     )
